9165949c2195994e9a228f82c9794a3bbad24234
Adlet <adlet.zh19966@gmail.com>
2023-02-24 13:00:50 +0600

    add bgImg and assets
    added background image and assets for bgImage and icons

--- Login page layout/ViewController.swift
+++ Login page layout/ViewController.swift
@@ -6,13 +6,31 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    private lazy var viewBackgroundImage: UIImageView = {
+       let imageName = "background"
+       let image = UIImage(named: imageName)
+       let imageView = UIImageView(image: image)
+       imageView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+       return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupHierarchy()
+        setupLayout()
     }
+    private func setupHierarchy() {
+        view.addSubview(viewBackgroundImage)
+    }
+    private func setupLayout() {
+        
+    }
+    
 
 
 }
