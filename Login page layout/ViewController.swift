88b8c9cf4b0c3ba342b2ba1da39fdba5eb5735c3
Adlet <adlet.zh19966@gmail.com>
2023-02-24 15:06:22 +0600

    add usernameTF/passwordTF

--- Login page layout/ViewController.swift
+++ Login page layout/ViewController.swift
@@ -24,6 +24,27 @@ class ViewController: UIViewController {
         loginLabel.font = UIFont(name: "Futura", size: CGFloat(30))
         return loginLabel
     }()
+    private lazy var usernameTextField: UITextField = {
+       let usernameTextField = UITextField()
+        usernameTextField.placeholder = "keanureeves01" + " "
+        usernameTextField.textAlignment = .center
+        usernameTextField.setLeftIcon(image: UIImage(systemName: "person.fill")!)
+        usernameTextField.setRightIcon(image: UIImage(systemName: "checkmark.circle.fill")!)
+        usernameTextField.backgroundColor = .white
+        usernameTextField.textColor = .black
+        usernameTextField.layer.cornerRadius = 25
+        return usernameTextField
+    }()
+    private lazy var passwordTextField: UITextField = {
+       let passwordTextField = UITextField()
+        passwordTextField.placeholder = "password"
+        passwordTextField.textAlignment = .center
+        passwordTextField.setLeftIcon(image: UIImage(systemName: "lock.fill")!)
+        passwordTextField.backgroundColor = .white
+        passwordTextField.textColor = .black
+        passwordTextField.layer.cornerRadius = 25
+        return passwordTextField
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,18 +55,45 @@ class ViewController: UIViewController {
     private func setupHierarchy() {
         view.addSubview(viewBackgroundImage)
         view.addSubview(loginLabel)
+        view.addSubview(usernameTextField)
+        view.addSubview(passwordTextField)
     }
     private func setupLayout() {
+        
         loginLabel.snp.makeConstraints { loginLabel in
             loginLabel.centerX.equalTo(view)
-            print(view.frame.height)
-            print(view.frame.height*0.117)
-            print(view.frame.width)
             loginLabel.top.equalTo(view).offset(view.frame.height*0.117)
         }
+        usernameTextField.snp.makeConstraints { usernameTextField in
+            usernameTextField.centerX.equalTo(view)
+            usernameTextField.height.equalTo(view.frame.height*0.058)
+            usernameTextField.top.equalTo(loginLabel.snp.bottom).offset(view.frame.height*0.0704)
+            usernameTextField.left.equalTo(view.snp.left).offset(view.frame.width*0.152)
+        }
+        passwordTextField.snp.makeConstraints { passwordTextField in
+            passwordTextField.centerX.equalTo(view)
+            passwordTextField.height.equalTo(view.frame.height*0.058)
+            passwordTextField.top.equalTo(usernameTextField.snp.bottom).offset(view.frame.height*0.0704)
+            passwordTextField.left.equalTo(view.snp.left).offset(view.frame.width*0.152)
+        }
     }
-    
-
-
 }
 
+extension UITextField {
+    func setLeftIcon(image: UIImage) {
+        let iconView = UIImageView(frame: CGRect(x: 10, y: 5, width: 20, height: 20))
+        iconView.image = image
+        let iconContainerView: UIView = UIView(frame: CGRect(x: 20, y: 0, width: 30, height: 30))
+        iconContainerView.addSubview(iconView)
+        leftView = iconContainerView
+        leftViewMode = .always
+    }
+    func setRightIcon(image: UIImage) {
+        let iconView = UIImageView(frame: CGRect(x: 0, y: 5, width: 20, height: 20))
+        iconView.image = image
+        let iconContainerView: UIView = UIView(frame: CGRect(x: 20, y: 0, width: 30, height: 30))
+        iconContainerView.addSubview(iconView)
+        rightView = iconContainerView
+        rightViewMode = .always
+    }
+}
