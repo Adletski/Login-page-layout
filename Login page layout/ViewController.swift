ec3646a7e8d488a29c941a7c04b771f9c0c81847
Adlet <adlet.zh19966@gmail.com>
2023-02-24 14:09:17 +0600

    add loginLabel/layout
    added loginLabel and layout for Login label

--- Login page layout/ViewController.swift
+++ Login page layout/ViewController.swift
@@ -17,6 +17,13 @@ class ViewController: UIViewController {
        imageView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
        return imageView
     }()
+    private lazy var loginLabel: UILabel = {
+       let loginLabel = UILabel()
+        loginLabel.text = "Login"
+        loginLabel.textColor = .white
+        loginLabel.font = UIFont(name: "Futura", size: CGFloat(30))
+        return loginLabel
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,9 +33,16 @@ class ViewController: UIViewController {
     }
     private func setupHierarchy() {
         view.addSubview(viewBackgroundImage)
+        view.addSubview(loginLabel)
     }
     private func setupLayout() {
-        
+        loginLabel.snp.makeConstraints { loginLabel in
+            loginLabel.centerX.equalTo(view)
+            print(view.frame.height)
+            print(view.frame.height*0.117)
+            print(view.frame.width)
+            loginLabel.top.equalTo(view).offset(view.frame.height*0.117)
+        }
     }
     
 
